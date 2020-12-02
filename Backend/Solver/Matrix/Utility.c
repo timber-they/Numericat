22bc1d258f6b6e0e7d90dc4680ee74e8d0ca115d
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-12-02 13:24:22 +0100

    replace long with double and add add some code for invert

--- Backend/Solver/Matrix/Utility.c
+++ Backend/Solver/Matrix/Utility.c
@@ -4,21 +4,26 @@
 typedef struct Matrix {
     int rowSize;
     int columnSize;
-    long int** matrix;
+    double **ptr = (double**)matrix;
 } Matrix;
+
+double GetRandomNumber(double minimum, double maximum)
+{ 
+    Random random = new Random();
+    return random.NextDouble() * (maximum - minimum) + minimum;
+}
 
 void randomize(Matrix *m){
     int i,j;
     for(i = 0; i < m->rowSize ; i++){
         for(j = 0; j < m->columnSize; j++){
-            m->matrix[i][j] = rand() % 5000;
+            m->matrix[i][j] = GetRandomNumber(0,10) % 5000;
         }
     }
 }
 
-
 Matrix createMatrix(int r, int c){
-    Matrix temp = {r, c, calloc(r, sizeof(long int *))};
+    Matrix temp = {r, c, calloc(r, sizeof(double *))};
 
     if (temp.matrix == NULL) {
         fprintf(stderr, "Empty Matrix not allowed\n");
@@ -48,7 +53,7 @@ void printMatrix(Matrix *m){
     int i,j;
     for(i = 0; i < m->rowSize ; i++){
         for(j = 0; j < m->columnSize; j++){
-            printf("%li ", *(m->matrix + i*m->rowSize  + j));
+            printf("%d ", *(m->matrix + i*m->rowSize  + j));
         }
         printf("\n");
     }
@@ -61,15 +66,15 @@ Matrix multiply(Matrix *a, Matrix *b){
         exit(0);
     }
 
-    int r = a->rowSize;
-    int c = b->columnSize;
+    double r = a->rowSize;
+    double c = b->columnSize;
     Matrix result = createMatrix(r,c);
 
     //multiply part
     int i,j;
     for(i = 0; i < r ; i++){
         for(j = 0; j < c; j++){
-            long int sum = 0;
+            long double sum = 0;
             int k;
             for(k = 0; k < a->columnSize; k++){
                 //sum += a[i][k] * b[k][j]
@@ -82,6 +87,39 @@ Matrix multiply(Matrix *a, Matrix *b){
     return result;
 }
 
+Matrix determinant(Matrix *a)
+{
+    float det, ratio;
+     for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            if(j>i){
+                ratio = a.matrix[j][i]/a.matrix[i][i];
+                for(k = 0; k < n; k++){
+                    a.matrix[j][k] -= ratio * a.matrix[i][k];
+                }
+            }
+        }
+    }
+    det = 1; //storage for determinant
+    for(i = 0; i < n; i++)
+        det *= a.matrix[i][i];
+    printf("Det is: %.2f\n\n", det);
+    return det;
+}
+
+
 Matrix invert(Matrix *a){
-    // TODO implement
+    for(i = 0; i < 3; i++){
+		for(j = 0; j < 3; j++)
+        {
+            a.matrix[i][j] = ((mat[(j+1)%3][(i+1)%3] * 
+                               mat[(j+2)%3][(i+2)%3]) - 
+                              (mat[(j+1)%3][(i+2)%3] * 
+                               mat[(j+2)%3][(i+1)%3]))/ 
+                               determinant(a));
+		
+		printf("\n");
+        }
+	}
+    return a.matrix    
 }
