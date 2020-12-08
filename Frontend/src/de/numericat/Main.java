c1f3cc4774c3054cd293a8de753523aa7cf523e0
metacolon <timo@teemze.de>
2020-12-08 20:37:27 +0100

    Create basic Swing application

--- Frontend/src/de/numericat/Main.java
+++ Frontend/src/de/numericat/Main.java
@@ -1,8 +1,25 @@
 package de.numericat;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // TODO: Figure out how to properly visualise in Java and do that here
+        createWindow();
+    }
+
+    private static void createWindow(){
+
+        //Create and set up the window
+        JFrame frame = new JFrame ("simple GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel textLabel = new JLabel("Im cool" ,SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+        //Display the window
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
