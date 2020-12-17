3cdcd8fbe0fea45db88436c105edb1c3dfec8874
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-12-17 17:51:38 +0100

    add type to test

--- Backend/Tests/Check_Matrix.h
+++ Backend/Tests/Check_Matrix.h
@@ -16,13 +16,13 @@ START_TEST(test_determinant)
     tmp.matrix[1][1] = 2;
     int dimension = getDimension(&tmp);
     int det = determinant(&tmp, dimension);
-    ck_assert(determinant = det);
+    ck_assert(determinant == det);
 }
 END_TEST
 
 START_TEST(test_Inverse)
 {
-    temp = createMatrix(2,2);
+    Matrix temp = createMatrix(2,2);
     temp.matrix[0][0] = 3;
     temp.matrix[0][1] = 2;
     temp.matrix[1][0] = 2;
@@ -37,17 +37,17 @@ END_TEST
 
 START_TEST(test_multiply)
 {
-    Matrix_1 = createMatrix(2,2);
+    Matrix Matrix_1 = createMatrix(2,2);
     Matrix_1.matrix[0][0] = 2;
     Matrix_1.matrix[0][1] = 2;
     Matrix_1.matrix[1][0] = 2;
     Matrix_1.matrix[1][1] = 2;
-    Matrix_2 = createMatrix(2,2);
+    Matrix Matrix_2 = createMatrix(2,2);
     Matrix_2.matrix[0][0] = 2;
     Matrix_2.matrix[0][1] = 2;
     Matrix_2.matrix[1][0] = 2;
     Matrix_2.matrix[1][1] = 2;
-    Matrix multiplied = multiply(Matrix_1, Matrix_2);
+    Matrix Matrix multiplied = multiply(Matrix_1, Matrix_2);
     ck_assert(multiplied.matrix[0][0] == 8);
     ck_assert(multiplied.matrix[1][0] == 8);
     ck_assert(multiplied.matrix[0][1] == 8);
