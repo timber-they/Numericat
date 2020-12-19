5e27e4f3e7e381057caa6d56ecbe4516cb983371
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-12-19 20:18:28 +0100

    fix  datatype

--- Backend/Tests/Check_Matrix.h
+++ Backend/Tests/Check_Matrix.h
@@ -5,19 +5,6 @@
 #ifndef BACKEND_UTILITY_CHECK_H
 #define BACKEND_UTILITY_CHECK_H
 
-START_TEST(test_det)
-{
-    Matrix tmp = createMatrix(2,2);
-    tmp.matrix[0][0] = 2.0;
-    tmp.matrix[0][1] = 2.0;
-    tmp.matrix[1][0] = 2.0;
-    tmp.matrix[1][1] = 2.0;
-    int dimension = getDimension(&tmp);
-    int det = determinant(&tmp, dimension);
-    ck_assert(det == 0);
-}
-END_TEST
-
 START_TEST(test_multiply)
 {
     Matrix Matrix_1 = createMatrix(2,2);
@@ -38,6 +25,36 @@ START_TEST(test_multiply)
     ck_assert(multiplied.matrix[1][1] == 8.0);
 }
 END_TEST
+
+START_TEST(test_det)
+{
+    Matrix tmp = createMatrix(2,2);
+    tmp.matrix[0][1] = 2.0;
+    tmp.matrix[1][0] = 2.0;
+    tmp.matrix[1][1] = 2.0;
+    tmp.matrix[0][0] = 2.0;
+    int dimension = getDimension(&tmp);
+    double det = Determinant(tmp.matrix, dimension);
+    ck_assert(det == 0.0);
+}
+END_TEST
+
+START_TEST(test_Inverse)
+{
+    Matrix temp = createMatrix(2,2);
+    temp.matrix[0][0] = 3.0;
+    temp.matrix[0][1] = 2.0;
+    temp.matrix[1][0] = 2.0;
+    temp.matrix[1][1] = 3.0;
+    Matrix Inverse = createMatrix(2,2);
+    invert(&temp, &Inverse);
+    ck_assert(Inverse.matrix[0][0] == 0.6);
+    ck_assert(Inverse.matrix[1][0] == -0.4);
+    ck_assert(Inverse.matrix[0][1] == -0.4);
+    ck_assert(Inverse.matrix[1][1] == 0.6);
+}
+END_TEST
+
 
 #endif
 
