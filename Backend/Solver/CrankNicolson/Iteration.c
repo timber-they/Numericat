082001ddf1b1ec3823f8c488359a6778ab820a1c
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-12-26 14:06:32 +0100

    add  Psi0 function

--- Backend/Solver/CrankNicolson/Iteration.c
+++ Backend/Solver/CrankNicolson/Iteration.c
@@ -2,7 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-void crank_nickelson()
+double _Eop = 938.27;
+double _hbarc = 0.1973;
+double _c = 3.0 * pow(10,2);
+
+double Psi0(double x);
+{
+    double A, K1,K2;
+    double x0 = -0.100;
+    double a = 0.050;
+    double l  = 200000.0
+
+    A = pow((1.0 / (2 * pi * pow(a,2))) , 0.25);
+    K1 = exp(- pow((x - x0), 2) / (4.0 * pow(a, 2)));
+    K2 = exp(1 * l * x);  
+    
+    return A * K1 * K2;
+}
+
+double deletaPotential()
+{
+    
+}
+
+double crank_nickelson()
 {
     
 }
