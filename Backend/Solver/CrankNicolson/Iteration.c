de94a3cb509dd713f123243ba40ba55d8674f91c
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2020-12-30 20:08:16 +0100

    add function template and header

--- Backend/Solver/CrankNicolson/Iteration.c
+++ Backend/Solver/CrankNicolson/Iteration.c
@@ -1,31 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "Iteration.h"
 
-double _Eop = 938.27;
-double _hbarc = 0.1973;
-double _c = 3.0 * pow(10,2);
 
-double Psi0(double x);
-{
-    double A, K1,K2;
-    double x0 = -0.100;
-    double a = 0.050;
-    double l  = 200000.0
+#define tMax 1
+#define dx (M_PI/10) 
+#define xf M_PI
+#define xi 0
+#define D 1.0
+#define F 0.0
+#define n 1.0
 
-    A = pow((1.0 / (2 * pi * pow(a,2))) , 0.25);
-    K1 = exp(- pow((x - x0), 2) / (4.0 * pow(a, 2)));
-    K2 = exp(1 * l * x);  
-    
-    return A * K1 * K2;
+#define lambda .4
+#define dt (lambda*dx*dx/D)
+
+void CR() {
+	int N = (int)((xf - xi) / dx), M = (int)(tMax / dt);
+	double u[M + 1][N + 1];
+	double a = 1 + lambda, b = -lambda / 2, c = -lambda / 2;
+	double alpha[N], g[N];
+
+
+	int  i, j, t;
+	double k, l;
+	for (i = 0; i <= N; i++)
+		u[0][i] = U0((double)(i*dx)); 			//initializing u0
+	for (i = 0; i <= M; i++) {				//initializing boundaries
+		u[i][0] = 0;
+		u[i][N] = 0;
+	}							//implementing tridiagonal method
+	alpha[0] = a;
+	for (t = 1; t <= M; t++) {
+		g[0] = ((1 - lambda)*u[t - 1][1] + (lambda / 2)*u[t - 1][0]);
+		for (i = 1; i < N; i++) {
+			alpha[i] = a - c*b / alpha[i - 1];
+			g[i] = ((lambda / 2)*u[t - 1][i - 1] + (1 - lambda)*u[t - 1][i] + (lambda / 2)*u[t - 1][i + 1]) - b / alpha[i - 1] * g[i - 1];
+		}
+
+		u[t][N - 1] = g[N - 1] / alpha[N - 1];
+
+		for (j = N - 2; j > 0; j--)
+			u[t][j] = (g[j] - c*u[t][j + 1]) / alpha[j];
+	}
+	FILE *ft = fopen("t.csv", "w"), *fx = fopen("x.csv", "w"), *festimate = fopen("estimate.csv", "w"),
+		*f_exact = fopen("exact.csv", "w"), *f_error = fopen("error.csv", "w");
+	double error = 0.0;
+	for (i = 0; i <= M; i++) {
+		for (j = 0; j <= N; j++) {
+			printf("%.8lf ", u[i][j]);
+			fprintf(festimate, "%.8lf,", u[i][j]);
+			if (i == 0 || i == M || j == 0 || j == N) {
+				error = 0;
+			}
+			else {
+				error = fabs((u[i][j] - (exact(j*dx, i*dt))) / u[i][j]) * 100;
+			}
+			fprintf(f_error, "%.8lf,", error);
+		}
+		printf("\n");
+		fprintf(festimate, "\n");
+		fprintf(f_error, "\n");
+	}
+	printf("\n\n");
+	for (k = 0; k <= N; k++) {
+		fprintf(ft, "%lf,", k);
+	}
+	for (k = 0; k <= M; k++) {
+		fprintf(fx, "%lf\n", k);
+	}
+	for (k = 0; k <= M; k++) {
+		for (l = 0; l <= N; l++) {
+			printf("%.8lf ", exact(l*dx, k*dt));
+			fprintf(f_exact, "%.8lf,", exact(l*dx, k*dt));
+		}
+		fprintf(f_exact, "\n");
+		printf("\n");
+	}
+	fclose(ft);
+	fclose(fx);
+	fclose(festimate);
+	fclose(f_error);
+	fclose(f_exact);
+
+}
+double U0(double x) {
+	return sin(n*x);
 }
 
-double deletaPotential()
-{
-    
-}
-
-double crank_nickelson()
-{
-    
+double exact(double x, double t) {
+	return exp(-n*n*t)*sin(n*x);
 }
---
+++ Backend/Solver/CrankNicolson/Iteration.h
@@ -0,0 +1,9 @@
+#ifndef BACKEND_NICKELSON_H
+#define BACKEND_NICKELSON_H
+
+
+double U0(double);
+double exact(double, double);
+void CrankNickelson();
+
+#endif // BACKEND_NICKELSON_H
