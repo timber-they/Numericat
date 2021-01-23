92cf95f7703c362f4a5b471a07ce5d75555602da
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2021-01-23 12:18:19 +0100

    Add Tests for Complex

--- Backend/Tests/Check_Complex.h
+++ Backend/Tests/Check_Complex.h
@@ -8,43 +8,73 @@
 
 START_TEST(test_sumComplex)
 {
+    printf("\n");
+    printf("test_sumComplex: \n");
     Complex a = (Complex) {.real = 1.1, .imaginary = 4.4};
+    printComplex(a);
     Complex b = (Complex) {.real = 4.4, .imaginary = 1.1};
+    printComplex(b);
     Complex result = sumComplex(a,b);
-    ck_assert((result.real - (5.5)) < 0.0001 && (result.real - (5.5)) > -0.0001);
+    printf("result: \n");
+    printComplex(result);
+    ck_assert((result.real - (5.5)) < 0.0001 && (result.imaginary - (5.5)) > -0.0001);
 }
 END_TEST
 
 START_TEST(test_subtractComplex)
 {
+    printf("\n");
+    printf("test_subtractComplex: \n");
     Complex a = (Complex) {.real = 4.4, .imaginary = 4.4};
+    printComplex(a);
     Complex b = (Complex) {.real = 1.1, .imaginary = 2.2};
+    printComplex(b);
     Complex result = subtractComplex(a,b);
-    ck_assert((result.real - (3.3)) < 0.0001 && (result.real - (2.2)) > -0.0001);
+    printf("result: \n");
+    printComplex(result);
+    ck_assert((result.real - (3.3)) < 0.0001 && (result.imaginary - (2.2)) > -0.0001);
 }
 END_TEST
 
 START_TEST(test_multiplyComplex)
 {
+    printf("\n");
+    printf("Complex Multiply Test: \n");
     Complex a = (Complex) {.real = 1.4, .imaginary = 3.3};
+    printComplex(a);
     Complex b = (Complex) {.real = 3.1, .imaginary = 2.2};
+    printComplex(b);
     Complex result = multiplyComplex(a,b);
-    ck_assert((result.real - (-2.92)) < 0.0001 && (result.real - (13.31)) > -0.0001);
+    printf("result: \n");
+    printComplex(result);
+    ck_assert((result.real + 2.920000) < 0.0001 && (result.imaginary - (13.310000)) > -0.0001);
 }
 END_TEST
 
 START_TEST(test_divideComplex)
 {
+    printf("\n");
+    printf("test_divideComplex: \n");
     Complex a = (Complex) {.real = 1.4, .imaginary = 3.3};
+    printComplex(a);
     Complex b = (Complex) {.real = 3.1, .imaginary = 2.2};
-    Complex result = multiplyComplex(a,b);
-    ck_assert((result.real - (0.80276)) < 0.0001 && (result.real - (0.49481)) > -0.0001);
+    printComplex(b);
+    Complex result = divideComplex(a,b);
+    printf("result: \n");
+    printComplex(result);
+    ck_assert((result.real - (0.80276)) < 0.0001 && (result.imaginary - (0.49481)) > -0.0001);
 }
 END_TEST
 
 START_TEST(test_absSquareComplex)
 {
-
+    printf("\n");
+    printf("test_absSquareComplex: \n");
+    Complex a = (Complex) {.real = 1.4, .imaginary = 3.3};
+    printComplex(a);
+    double result = absSquareComplex(a);
+    printf("result: %lf\n", result);
+    ck_assert((result - (12.85)) < 0.0001);
 }
 END_TEST
 
--- Backend/Tests/Checks.c
+++ Backend/Tests/Checks.c
@@ -12,6 +12,7 @@ int main(void)
     TCase *parserCase1 = tcase_create("Parser");
     TCase *parserCase2 = tcase_create("Function");
     TCase *solverCase1 = tcase_create("Solver");
+    TCase *complexCase1 = tcase_create("Complex");
 
     SRunner *sr = srunner_create(parserSuite);
     srunner_add_suite(sr, solverSuite);
@@ -20,6 +21,7 @@ int main(void)
     suite_add_tcase(parserSuite, parserCase1);
     suite_add_tcase(parserSuite, parserCase2);
     suite_add_tcase(solverSuite, solverCase1);
+    suite_add_tcase(solverSuite, complexCase1);
 
     tcase_add_test(parserCase1, test_Parse_Functions);
     tcase_add_test(parserCase1, test_Parse_Failing);
@@ -35,6 +37,11 @@ int main(void)
     tcase_add_test(solverCase1, test_thomas);
     tcase_add_test(solverCase1, test_sumComplexMatrix);
     tcase_add_test(solverCase1, test_ComplexmultiplyMatrix);
+    tcase_add_test(complexCase1, test_sumComplex);
+    tcase_add_test(complexCase1, test_subtractComplex);
+    tcase_add_test(complexCase1, test_multiplyComplex);
+    tcase_add_test(complexCase1, test_divideComplex);
+    tcase_add_test(complexCase1, test_absSquareComplex);
 
 
     srunner_run_all(sr, CK_NORMAL);
