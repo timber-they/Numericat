e8700cb69e1d006ffa63f18d1fcb92abdce55cdf
MetaColon <timo@teemze.de>
2021-02-06 14:36:30 +0100

    Remove TODO

--- Backend/Solver/Matrix/Complex.h
+++ Backend/Solver/Matrix/Complex.h
@@ -10,7 +10,6 @@ Complex sumComplex(Complex a, Complex b);
 Complex subtractComplex(Complex a, Complex b);
 Complex multiplyComplex(Complex a, Complex b);
 Complex divideComplex(Complex a, Complex b);
-// TODO: Unit test
 Complex powerComplex(Complex a, Complex b);
 Complex expComplex(Complex b);
 double absSquareComplex(Complex c);
