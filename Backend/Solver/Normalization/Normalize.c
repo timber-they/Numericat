7b1fbd4ce95a8f9d63b9af436b65b9bcaf1a70d3
Herrmann, Tom <tom.herrmann@studium.uni-hamburg.de>
2021-02-13 20:19:28 +0100

    remove Normalize.c Normalize.h

--- Backend/Solver/Normalization/Normalize.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by Tom on 09.02.21.
-//
-
--- Backend/Solver/Normalization/Normalize.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Tom on 09.02.21.
-//
-
-#ifndef NUMERICAT_NORMALIZE_H
-#define NUMERICAT_NORMALIZE_H
-
-#endif //NUMERICAT_NORMALIZE_H
